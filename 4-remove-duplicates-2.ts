77727b42d0579f60f8f7a7af8ab5139048b0d0e9
joao gugel <ojoaogugel@gmail.com>
2024-12-13 11:01:23 -0300

    4: remove duplicates medium

---
+++ 4-remove-duplicates-2.ts
@@ -0,0 +1,13 @@
+function removeDuplicates(nums: number[]): number {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[k - 2] === nums[i]) continue;
+    nums[k] = nums[i];
+    k++;
+  }
+
+  return k;
+}
+
+console.log(removeDuplicates([1, 1, 1, 2, 2, 3]));
