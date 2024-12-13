34dabb544fef15fbd412006377e02c3bfde3c5f1
joao gugel <ojoaogugel@gmail.com>
2024-12-13 09:52:26 -0300

    fix merge sorted arrays

--- 1-merge-sorted-array.ts
+++ 1-merge-sorted-array.ts
@@ -6,14 +6,22 @@ let n = 3;
 // Alterar array original, sorteando os 2 arrays.
 
 function merge(nums1: number[], m: number, nums2: number[], n: number) {
-  const filteredNums1 = nums1.filter((num) => num);
-  const filteredNums2 = nums2.filter((num) => num);
+  let i = m - 1;
+  let j = n - 1;
+  let lastPos = m + n - 1;
 
-  const newArr = [...filteredNums1, ...filteredNums2].sort((a, b) => a - b);
-
-  for (let i = 0; i < nums1.length; i++) {
-    nums1[i] = newArr[i];
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[lastPos] = nums1[i];
+      i--;
+    } else {
+      nums1[lastPos] = nums2[j];
+      j--;
+    }
+    lastPos--;
   }
 }
 
 merge(nums1, m, nums2, n);
+
+console.log(nums1);
